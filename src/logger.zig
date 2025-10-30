a56352935e4aa4390a642eff24e8deda2c1ad235
Mariuspersen <marius_kule@hotmail.com>
2025-10-30 12:06:31 +0100

    simplify messy logger

--- src/logger.zig
+++ src/logger.zig
@@ -1,18 +1,10 @@
 const std = @import("std");
 const config = @import("config.zon");
+const Writer = std.io.Writer;
 
 const Self = @This();
 
-stdout: *std.Io.Writer = undefined,
-stdout_buf: [1024]u8 = undefined,
-stdout_writer: std.fs.File.Writer = undefined,
-stderr: *std.Io.Writer = undefined,
-stderr_writer: std.fs.File.Writer = undefined,
-stderr_buf: [1024]u8 = undefined,
-logger: *std.Io.Writer = undefined,
 logfile: std.fs.File,
-logfile_writer: std.fs.File.Writer = undefined,
-logfile_writer_buf: [1024]u8 = undefined,
 
 pub fn init() !Self {
     var name_buf: [1024]u8 = undefined;
@@ -30,61 +22,37 @@ pub fn init() !Self {
         else => return e,
     };
 
-    var self: Self = .{
+    return .{
         .logfile = try std.fs.cwd().createFile(name_buf[0..name_writer.end], .{}),
     };
-    self.stdout_writer = std.fs.File.stdout().writer(&self.stdout_buf);
-    self.stderr_writer = std.fs.File.stdout().writer(&self.stderr_buf);
-    self.stdout = &self.stdout_writer.interface;
-    self.stderr = &self.stderr_writer.interface;
-    self.logfile_writer = self.logfile.writer(&self.logfile_writer_buf);
-    self.logger = &self.logfile_writer.interface;
-    return self;
 }
 
 pub fn print_error(self: *Self, err: anyerror) void {
     const ts = std.time.timestamp();
     const en = @errorName(err);
     const fmt = "{d}: ERROR: {s}\n";
-    const err_fmt = "{d}: ERROR: Unable to print error {s}\n";
     const args = .{ ts, en };
-    self.stderr.print(fmt, args) catch {
-        self.logger.print(err_fmt, args) catch {};
-    };
-    self.logger.print(fmt, args) catch {
-        self.stderr.print(err_fmt, args) catch {};
-    };
-    self.flush_error() catch {};
+    const writers = [_]std.fs.File{ self.logfile, std.fs.File.stderr() };
+    for (writers) |f| {
+        var buf: [1024]u8 = undefined;
+        var w = f.writer(&buf);
+        const out = &w.interface;
+
+        out.print(fmt,args) catch {};
+        out.flush() catch {};
+    }
 }
 
 pub fn print(self: *Self, comptime fmt: []const u8, args: anytype) void {
-    self.logger.print(fmt, args) catch |e| self.print_error(e);
-    self.stdout.print(fmt, args) catch |e| self.print_error(e);
-}
+    const writers = [_]std.fs.File{ self.logfile, std.fs.File.stdout() };
+    for (writers) |f| {
+        var buf: [1024]u8 = undefined;
+        var w = f.writer(&buf);
+        const out = &w.interface;
 
-pub fn log_request(self: *Self, address: *const std.net.Address, path: []const u8) void {
-    const ts = std.time.timestamp();
-    const fmt = "{d}: {s} => ";
-    const args = .{
-        ts, path,
-    };
-    self.logger.print(fmt, args) catch |e| self.print_error(e);
-    self.stdout.print(fmt, args) catch |e| self.print_error(e);
-    address.format(self.logger) catch |e| self.print_error(e);
-    address.format(self.stdout) catch |e| self.print_error(e);
-    self.logger.writeAll(config.newline) catch |e| self.print_error(e);
-    self.stdout.writeAll(config.newline) catch |e| self.print_error(e);
-    self.flush();
-}
-
-fn flush_error(self: *Self) !void {
-    try self.logger.flush();
-    try self.stderr.flush();
-}
-
-pub fn flush(self: *Self) void {
-    self.logger.flush() catch |e| self.print_error(e);
-    self.stdout.flush() catch |e| self.print_error(e);
+        out.print(fmt,args) catch |e| self.print_error(e);
+        out.flush() catch |e| self.print_error(e);
+    }
 }
 
 pub fn deinit(self: *Self) void {
--- src/main.zig
+++ src/main.zig
@@ -3,8 +3,10 @@ const Logger = @import("logger.zig");
 const config = @import("config.zon");
 
 const linux = std.os.linux;
+const posix = std.posix;
 const RespondOptions = std.http.Server.Request.RespondOptions;
 const Connection = std.net.Server.Connection;
+const Address = std.net.Address;
 
 fn hash(bytes: []const u8) u64 {
     var final: u64 = 0;
@@ -16,16 +18,28 @@ fn hash(bytes: []const u8) u64 {
     return final;
 }
 
-const address = std.net.Address.parseIp4(
+fn genToken(addr: []const u8) ![32]u8 {
+    var sha = std.crypto.hash.sha3.Sha3_256.init(.{});
+    sha.update(addr);
+    var final: [32]u8 = undefined;
+    sha.final(&final);
+    return final;
+}
+
+test "Generate Tokens" {
+    genToken(address);
+}
+
+const address = Address.parseIp4(
     config.ip,
     config.port,
 ) catch |err| @compileError(err);
 
-const options = std.net.Address.ListenOptions{
+const options = Address.ListenOptions{
     .reuse_address = true,
 };
 
-pub fn main() !void { 
+pub fn main() !void {
     var server = try address.listen(options);
     defer server.deinit();
 
@@ -33,7 +47,6 @@ pub fn main() !void {
     defer logger.deinit();
 
     logger.print("Listening at http://{s}:{d}\n", .{ config.ip, config.port });
-    logger.flush();
 
     while (true) {
         const connection = server.accept() catch |e| {
@@ -57,6 +70,18 @@ pub fn main() !void {
             continue;
         };
 
+        var address_buf: [1024]u8 = undefined;
+        var address_writer = std.io.Writer.fixed(&address_buf);
+        connection.address.format(&address_writer) catch |e| {
+            logger.print_error(e);
+            continue;
+        };
+        const index = std.mem.lastIndexOf(u8, &address_buf, ":") orelse {
+            logger.print_error(error.AddressNotHaveAColon);
+            continue;
+        };
+        const address_str = address_buf[0..index];
+
         var it = std.mem.splitAny(u8, request.head.target, "?");
         const path = it.next() orelse request.head.target;
 
@@ -64,7 +89,11 @@ pub fn main() !void {
         const result = switch (hashid) {
             hash("/") => request.respond(
                 @embedFile("assets/index.html"),
-                .{},
+                .{
+                    .extra_headers = &.{
+                        .{ .name = "Token", .value = &try genToken(address_str) },
+                    },
+                },
             ),
             hash("/style.css") => request.respond(
                 @embedFile("assets/style.css"),
@@ -105,6 +134,8 @@ pub fn main() !void {
             continue;
         };
 
-        logger.log_request(&connection.address, path);
+        logger.print("{d}: {s} => {s}\n", .{
+            std.time.timestamp(), path, address_str,
+        });
     }
 }
