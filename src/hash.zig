154c6e9dc45574dea8d656f76d9e527f0544f84f
Marius Alexandersen <marius_kule@hotmail.com>
2025-11-18 17:48:33 +0100

    better hash

--- src/hash.zig
+++ src/hash.zig
@@ -1,10 +1,10 @@
 pub fn hash(bytes: []const u8) u64 {
-    var final: u64 = 0;
+    var final: u64 = 0x9e3779b185ebca87;
     @setEvalBranchQuota(3000);
-    for (bytes, 1..) |b, i| {
-        const mul = @mulWithOverflow(b, i)[0];
-        const res = @addWithOverflow(final, mul)[0];
-        final = res;
+    for (bytes) |b| {
+        final ^= b;
+        final = @mulWithOverflow(final, 0x165667919e3779f9)[0];
+        final ^= final >> 27;
     }
     return final;
 }
