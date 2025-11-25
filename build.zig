a6f0f69a1d1a666f429e3c109a05a98a7ee1f0d0
Mariuspersen <marius_kule@hotmail.com>
2025-11-25 15:35:33 +0100

    Start of modifications to build.zig and other

--- build.zig
+++ build.zig
@@ -27,23 +27,40 @@ pub fn build(b: *std.Build) void {
     });
     switchgen.root_module.addImport("hash", hash_mod);
 
+    var runs = std.ArrayList(?*std.Build.Step.Run).initCapacity(b.allocator, config.websites.len) catch @panic("OOM");
+    runs.deinit(b.allocator);
+
+    inline for (config.websites) |website| {
+        const run = if (std.fs.cwd().access("src/" ++ website.repo, .{})) null else |_| b.addSystemCommand(&.{
+            "git",
+            "clone",
+            "--recurse-submodules",
+            website.repo,
+            "src/" ++ website.repo,
+        });
+        runs.appendAssumeCapacity(run);
+    }
+
     const git = if (std.fs.cwd().access("src/assets", .{})) null else |_| b.addSystemCommand(&.{
         "git",
         "clone",
         "--recurse-submodules",
-        config.repo,
+        config.websites[0].repo,
         "src/assets",
     });
 
     const switchgen_step = b.addRunArtifact(switchgen);
+    for (runs.items) |run| if(run) |r| {
+        switchgen_step.step.dependOn(&r.step);
+    };
     if (git) |g| {
         switchgen_step.step.dependOn(&g.step);
     }
 
-    const slash_idx = if (std.mem.lastIndexOf(u8, config.repo, "/")) |i| i+1 else 0;
-    
+    const slash_idx = if (std.mem.lastIndexOf(u8, config.websites[0].repo, "/")) |i| i + 1 else 0;
+
     const exe = b.addExecutable(.{
-        .name = config.repo[slash_idx..],
+        .name = config.websites[0].repo[slash_idx..],
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
